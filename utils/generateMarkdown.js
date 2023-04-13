3bf8b25644ab94ea18abd4c852c41b3c0074ab02
Voi <wojtek.wacek.dola@gmail.com>
2023-04-13 17:35:55 +1000

    Created licenseArray for the license selection.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,45 @@
+const licenseArray = [
+  {
+    name: "Apache License 2.0",
+    badge: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    link: "https://opensource.org/licenses/Apache-2.0",
+  }
+  {
+    name: "GNU General Public License v3.0",
+    badge: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    link: "https://www.gnu.org/licenses/gpl-3.0",
+  }
+  {
+    name: "MIT License",
+    badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    link: "https://opensource.org/licenses/MIT",
+  }
+  {
+    name: "ISC License",
+    badge: "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+    link: "https://opensource.org/licenses/ISC",
+  }
+  {
+    name: "Mozilla Public License 2.0",
+    badge: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+    link: "https://opensource.org/licenses/MPL-2.0",
+  }
+  {
+    name: "Boost Software License 1.0",
+    badge: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
+    link: "https://www.boost.org/LICENSE_1_0.txt",
+  }
+  {
+    name: "The Unlicense",
+    badge: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+    link: "http://unlicense.org/",
+  }
+];
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  
   license === "I don’t want a license."
     ? (licenseBadge = "")
     : (licenseBadge = license);
