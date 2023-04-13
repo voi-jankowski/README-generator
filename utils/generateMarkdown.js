72740a49669aebe3545cfd7dc4e566ef1be11232
Voi <wojtek.wacek.dola@gmail.com>
2023-04-14 09:06:49 +1000

    Created license functions in generateMarkdown.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,65 +1,90 @@
 const licenseArray = [
   {
     name: "Apache License 2.0",
-    badge: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    badge:
+      "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
     link: "https://opensource.org/licenses/Apache-2.0",
-  }
+  },
   {
     name: "GNU General Public License v3.0",
-    badge: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    badge:
+      "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
     link: "https://www.gnu.org/licenses/gpl-3.0",
-  }
+  },
   {
     name: "MIT License",
-    badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    badge:
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
     link: "https://opensource.org/licenses/MIT",
-  }
+  },
   {
     name: "ISC License",
-    badge: "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+    badge:
+      "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
     link: "https://opensource.org/licenses/ISC",
-  }
+  },
   {
     name: "Mozilla Public License 2.0",
-    badge: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+    badge:
+      "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
     link: "https://opensource.org/licenses/MPL-2.0",
-  }
+  },
   {
     name: "Boost Software License 1.0",
-    badge: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
+    badge:
+      "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
     link: "https://www.boost.org/LICENSE_1_0.txt",
-  }
+  },
   {
     name: "The Unlicense",
-    badge: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+    badge:
+      "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
     link: "http://unlicense.org/",
-  }
+  },
+  {
+    name: "I don’t want a license.",
+    badge: "",
+    link: "",
+  },
 ];
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
-  license === "I don’t want a license."
-    ? (licenseBadge = "")
-    : (licenseBadge = license);
-  renderLicenseLink(licenseBadge);
+  const licenseFound = licenseArray.find((obj) => obj.name === license);
+
+  const licenseBadge = licenseFound.badge;
+
+  renderLicenseLink(licenseFound);
+  // for (const licenseType of licenseArray) {
+  //   licenseType.name === license ? licenseBadge = licenseType.badge : return;
+  // }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenseFound) {
+  const licenseLink = licenseFound.link;
+
+  renderLicenseSection(licenseFound);
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(licenseFound) {
+  const licenseName = licenseFound.name;
 
+  const licenseSection = `This application is covered under [${licenseName}](${licenseLink})`;
+}
+console.log(licenseBadge);
+console.log(licenseLink);
+console.log(licenseSection);
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   renderLicenseBadge(data.license);
   return `# ${data.title}
 
-${data.licenseBadge}
+${licenseBadge}
 
 ## Description
 
@@ -89,9 +114,7 @@ ${data.credits}
 
 ## License
 
-The application is covered under ${data.license}
-
-Please refer to the LICENSE in the repo.
+${licenseSection}
 
 ## Contributing
 
