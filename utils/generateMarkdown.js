649b6d755b689d81ed8d413552f1099dba2d34ec
Voi <wojtek.wacek.dola@gmail.com>
2023-04-15 09:42:12 +1000

    Included the credit for the source of licence badges.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// Array of possible licences with badge and link properties.
+// Array of possible licences with badge and link properties. The list is compiled with the aid of the source at https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
 const licenseArray = [
   {
     name: "Apache License 2.0",
